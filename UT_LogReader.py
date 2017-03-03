954aa4180e1b9fae373db60747096b4eae70e1a2
DidacMC <didac.magrina@gmail.com>
2017-03-03 17:42:20 +0000

    LogReader basic file and UT added

---
+++ UT_LogReader.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      didac
+#
+# Created:     03/03/2017
+# Copyright:   (c) didac 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+import LogReader
+
+def main():
+	pass
+
+if __name__ == '__main__':
+    main()
